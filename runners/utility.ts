8745ab2bfd03c8c00eb5c1a50bc9b6e9417597cc
j50n <j50n@lilypepper.com>
2022-03-17 11:23:55 -0600

    debug code; set buffer size to small

--- runners/utility.ts
+++ runners/utility.ts
@@ -179,7 +179,7 @@ export async function* readerToBytesUnbuffered(
   const trace: number[] = [];
   try {
     while (true) {
-      const buffer = new Uint8Array(DEFAULT_BUFFER_SIZE);
+      const buffer = new Uint8Array(100);
       const len = await reader.read(buffer);
       if (len === null) {
         break;
