0267c302664f97efb627a288114ad318e9358d6b
j50n <j50n@lilypepper.com>
2023-05-21 06:48:01 -0600

    30 move all existing code and documentation to legacy/ folder

---
+++ legacy/mod.ts
@@ -0,0 +1,22 @@
+export * from "./runner.ts";
+export * from "./runners/proc-group.ts";
+export * from "./runners/handlers/string.ts";
+export * from "./runners/handlers/string-array.ts";
+export * from "./runners/handlers/bytes.ts";
+export * from "./runners/handlers/reader.ts";
+export * from "./runners/handlers/string-asynciterable.ts";
+export * from "./runners/handlers/bytes-asynciterable.ts";
+export * from "./runners/handlers/empty.ts";
+export * from "./runners/handlers/stderr-to-stdout-string-asynciterable.ts";
+export * from "./runners/closers.ts";
+export * from "./runners/process-exit-error.ts";
+export * from "./runners/chained-error.ts";
+export * from "./runners/stderr-support.ts";
+export * from "./runners/push-iterable.ts";
+export {
+  bytesToTextLines,
+  dirname,
+  readerToBytes,
+  readerToBytesUnbuffered,
+  sleep,
+} from "./runners/utility.ts";
