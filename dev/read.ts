9d5aeadb0aed204a9595c786810a1c1948d75657
j50n <j50n@lilypepper.com>
2024-01-15 07:02:34 -0700

    site docs

--- dev/read.ts
+++ dev/read.ts
@@ -1,4 +1,4 @@
-import { enumerate } from "https://deno.land/x/proc@0.21.8/mod.ts";
+import { enumerate } from "https://deno.land/x/proc@0.21.9/mod.ts";
 
 for await (const line of enumerate(Deno.stdin.readable).lines) {
   console.log(line);
