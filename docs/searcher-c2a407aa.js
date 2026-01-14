1306653c3ee0ed70f2c1c1221e7ff479db7137c5
j50n <j50n@lilypepper.com>
2026-01-13 20:58:25 -0700

    Fix gitv plugin to replace all {{gitv}} patterns with single replaceAll

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ee147872.js',
+                path_to_root + 'searchindex-22b07831.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
