1eef7e2840c3491870cf559713777d0d55e27f27
j50n <j50n@lilypepper.com>
2026-01-14 08:13:57 -0700

    docs: rebuild documentation

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-59ecbe58.js',
+                path_to_root + 'searchindex-5e8f8e62.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
