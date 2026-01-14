9a98815c649fb2defb17c9d3dbfb429625a67a98
j50n <j50n@lilypepper.com>
2026-01-14 13:29:52 -0700

    build: regenerate site with experimental warnings

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5e8f8e62.js',
+                path_to_root + 'searchindex-0b2c0953.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
