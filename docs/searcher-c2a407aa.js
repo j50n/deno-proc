7d5c71d187b6ae5119850330f6ea3dc36db31c06
j50n <j50n@lilypepper.com>
2026-01-15 17:49:55 -0700

    docs: rebuild site

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0b2c0953.js',
+                path_to_root + 'searchindex-2222309c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
